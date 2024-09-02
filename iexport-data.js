48e47a03eeeced38b8f9f734028bf03b1ad71bf5
DanPENG <147044969+DAN-Pd@users.noreply.github.com>
2024-09-03 01:27:15 +0800

    Add files via upload

---
+++ iexport-data.js
@@ -0,0 +1,432 @@
+const IExportYears = [
+	2013,
+	2014,
+	2015,
+	2016,
+	2017,
+	2018,
+	2019,
+	2020
+];
+
+const IExportData={
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2013,
+				"name": "俄罗斯从中国进口",
+				"number": 4959117,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2014,
+				"name": "俄罗斯从中国进口",
+				"number": 5367694,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2015,
+				"name": "俄罗斯从中国进口",
+				"number": 3475688,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2016,
+				"name": "俄罗斯从中国进口",
+				"number": 3735577,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2017,
+				"name": "俄罗斯从中国进口",
+				"number": 4283060,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2018,
+				"name": "俄罗斯从中国进口",
+				"number": 4796527,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2019,
+				"name": "俄罗斯从中国进口",
+				"number": 4974849,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2020,
+				"name": "俄罗斯从中国进口",
+				"number": 5050447,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [120.001228,51.44174]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2013,
+				"name": "俄罗斯对中国出口",
+				"number": 3966783,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2014,
+				"name": "俄罗斯对中国出口",
+				"number": 4159351,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2015,
+				"name": "俄罗斯对中国出口",
+				"number": 3325866,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2016,
+				"name": "俄罗斯对中国出口",
+				"number": 3226015,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2017,
+				"name": "俄罗斯对中国出口",
+				"number": 4139029,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2018,
+				"name": "俄罗斯对中国出口",
+				"number": 5914218,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2019,
+				"name": "俄罗斯对中国出口",
+				"number": 6119171,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2020,
+				"name": "俄罗斯对中国出口",
+				"number": 5768468,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [131.098831,49.976778]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2013,
+				"name": "蒙古从中国进口",
+				"number": 244959,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2014,
+				"name": "蒙古从中国进口",
+				"number": 221638,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2015,
+				"name": "蒙古从中国进口",
+				"number": 157070,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2016,
+				"name": "蒙古从中国进口",
+				"number": 98864,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2017,
+				"name": "蒙古从中国进口",
+				"number": 123561,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2018,
+				"name": "蒙古从中国进口",
+				"number": 164489,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2019,
+				"name": "蒙古从中国进口",
+				"number": 182659,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2020,
+				"name": "蒙古从中国进口",
+				"number": 161807,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [101.387196,42.295524]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2013,
+				"name": "蒙古对中国出口",
+				"number": 350955,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2014,
+				"name": "蒙古对中国出口",
+				"number": 510209,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2015,
+				"name": "蒙古对中国出口",
+				"number": 379538,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2016,
+				"name": "蒙古对中国出口",
+				"number": 362260,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2017,
+				"name": "蒙古对中国出口",
+				"number": 516731,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2018,
+				"name": "蒙古对中国出口",
+				"number": 634411,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2019,
+				"name": "蒙古对中国出口",
+				"number": 633436,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		},
+        {
+			"type": "Feature",
+			"properties": {
+				"color": "#A9907E",
+				"year": 2020,
+				"name": "蒙古对中国出口",
+				"number": 512456,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.43212,42.731081]
+			}
+		}
+	]
+};
